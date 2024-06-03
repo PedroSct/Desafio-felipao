845ff6fead54a530164884c3c5cec7e6830c5807
PedroSct <pedroaugusto06082004@gmail.com>
2024-06-02 21:00:36 -0300

    Commit do desafio fo felipao

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("ola mundo");
+let nome = "Super Bigóde"
+let xp = 1000000
+
+
+if (xp <= 1000){
+    console.log(" O Herói de nome " + nome + " está no nivel de Ferro")
+} else if (xp > 1001 && xp <= 2000){
+    console.log(" O Herói de nome " + nome + " está no nivel de Bronze")
+} else if (xp > 2001 && xp <= 5000){
+    console.log(" O Herói de nome " + nome + " está no nivel de Prata")
+} else if (xp > 5001 && xp <= 7000){
+    console.log(" O Herói de nome " + nome + " está no nivel de Ouro")
+} else if (xp > 7001 && xp <= 8000){
+    console.log(" O Herói de nome " + nome + " está no nivel de Platina")
+} else if (xp > 8000 && xp <= 9000){
+    console.log(" O Herói de nome " + nome + " está no nivel de Ascendente")
+} else if (xp > 9001 && xp <= 10000){
+    console.log(" O Herói de nome " + nome + " está no nivel de Imortal")
+} else if (xp > 10001){
+    console.log("O Herói de nome " + nome + " está no ivel de Radiante")
+}
+
